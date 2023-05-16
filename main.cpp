d4e604cf772df8a766c40bd7f808d3d6aacfa66b
babynaz <132637847+babynaz@users.noreply.github.com>
2023-05-16 08:49:50 +0000

    done - 16/5/23

--- main.cpp
+++ main.cpp
@@ -1,33 +1,37 @@
-#define SIZE 10
 #include <iostream>
 #include <string.h>
 #include <iomanip>
 
 using namespace std;
 
-int main(void){
+int main(void)
+{
   char *pa, *pb, i, temp;
-  char a[SIZE] ;
+  char a[10000] ;
     cout<<"Input string: ";
     cin>>a;
+    int SIZE = strlen(a);
   
   pa = &a[0]; //pa = a;
   cout<< "Original: ";
-  for (int i = 0; i < SIZE - 1; i++, pa++){
+  for (int i = 0; i < SIZE - 1; i++, pa++)
+  {
     cout<<setw(3)<<*pa<<" ";
     cout<<pa<<endl;
   }
 
     pa = &a[0]; pb = &a[SIZE - 1];
     cout<<"Reverse: ";
-    for (int i = 0; i < SIZE / 2; i++) {
+    for (int i = 0; i < SIZE / 2; i++) 
+    {
         temp = *pa;
         *pa = *pb;
         *pb = temp;
         pa++; pb--;
     }
     pa = &a[0];
-    for (int i = 0; i < SIZE - 1; i++,pa++){
+    for (int i = 0; i < SIZE - 1; i++,pa++)
+    {
         cout<<setw(3)<<*pa<<" ";
         cout<<pa<<endl;
     }
