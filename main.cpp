0ae294301e750aaa3a6cf80b886e721a5ede83ea
babynaz <132637847+babynaz@users.noreply.github.com>
2023-05-16 08:44:03 +0000

    Committed - 16/5/23

--- main.cpp
+++ main.cpp
@@ -1,40 +1,36 @@
+#define SIZE 10
 #include <iostream>
+#include <string.h>
+#include <iomanip>
+
 using namespace std;
 
-int main(){
-
-  int a;
-  double d;
-  cout<<&a<<" size "<<sizeof(a) <<endl;
-  cout<<&d<<" size "<<sizeof(d) <<endl; 
+int main(void){
+  char *pa, *pb, i, temp;
+  char a[SIZE] ;
+    cout<<"Input string: ";
+    cin>>a;
   
-  /*  int x,*p;
-    p=&x;
-    x=5;
+  pa = &a[0]; //pa = a;
+  cout<< "Original: ";
+  for (int i = 0; i < SIZE - 1; i++, pa++){
+    cout<<setw(3)<<*pa<<" ";
+    cout<<pa<<endl;
+  }
 
-    cout<< x  <<" "<< *p <<endl;
-    cout<< &x <<" "<<  p <<endl;
-    cout<< &p <<endl;*/
-
-/*Example Array*/
-  /*
-int *ip;
-int a[ ] = {31415, 21828};
-ip = a;
-// alternative:	ip = &a[0];
-
-cout<<a<<endl;			
-cout<<ip<<" "<<*ip<<endl;	
-  
-(*ip)++; 
-cout<<a<<" "<<a[0]<<endl;		
-cout<<ip<<" "<<*ip<<endl; 	
-  
-ip++; 
-cout<<a<<endl;		
-cout<<ip<<" "<<*ip<<endl; 	// print 
-*/
- /*the address is 4324, not 4321
- because the pointer is incremented 						
- by the size of an integer (4 bytes) */
+    pa = &a[0]; pb = &a[SIZE - 1];
+    cout<<"Reverse: ";
+    for (int i = 0; i < SIZE / 2; i++) {
+        temp = *pa;
+        *pa = *pb;
+        *pb = temp;
+        pa++; pb--;
+    }
+    pa = &a[0];
+    for (int i = 0; i < SIZE - 1; i++,pa++){
+        cout<<setw(3)<<*pa<<" ";
+        cout<<pa<<endl;
+    }
+    
+return 0;
 }
